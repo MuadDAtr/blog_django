5623e3a5999ecab8f620b15947478cfcd1ac1bec
MuadDAtr <int.stron@protonmail.com>
2022-11-29 23:05:16 +0100

    settings

--- blog_py/blog_py/settings.py
+++ blog_py/blog_py/settings.py
@@ -119,9 +119,10 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
+    os.path.join(BASE_DIR, 'static_root')
 ]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static/')]
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media_root/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
